0630763d5e70c3450b5ec08a6e6764efdacb0f9b
puyol <puyolgt@gmail.com>
2021-06-28 15:53:03 +0900

    スタイルの追加

--- keiba_forecast/settings.py
+++ keiba_forecast/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'keiba_forecasts',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- keiba_forecasts/forms.py
+++ keiba_forecasts/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class RaceForm(forms.Form):
-    year = forms.CharField(label='year', max_length=4)
-    place = forms.CharField(label='開催', max_length=4)
-    num = forms.CharField(label='回', max_length=2)
-    day = forms.CharField(label='日目', max_length=2)
-    race = forms.CharField(label='R', max_length=2)
+    year = forms.CharField(label='year', max_length=4, initial='2021')
+    place = forms.CharField(label='開催', max_length=4, initial='東京')
+    num = forms.CharField(label='回', max_length=2, initial='1')
+    day = forms.CharField(label='日目', max_length=2, initial='1')
+    race = forms.CharField(label='R', max_length=2, initial='1')
